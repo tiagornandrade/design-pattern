50781af2ac2571d59da4e7703e5c37c19e21b5f2
Tiago Navarro <tiagornandrade@hotmail.com>
2023-02-08 18:36:44 -0300

    include facade javascript

--- structural/facade/javascript/index.ts
+++ structural/facade/javascript/index.ts
@@ -1,5 +1,10 @@
-import { CoffeeShop } from "./facade";
+import { Facade, Subsystem1, Subsystem2 } from "./subsystems"
 
-const coffeeShop = new CoffeeShop()
+function clientCode(facade: Facade) {
+    console.log(facade.operation());
+}
 
-coffeeShop.order()
+const subsystem1 = new Subsystem1();
+const subsystem2 = new Subsystem2();
+const facade = new Facade(subsystem1, subsystem2);
+clientCode(facade);
