4e47de716fa993877edb15f80e2fbe1069146a41
Tiago Navarro <tiagornandrade@hotmail.com>
2023-02-08 15:09:57 -0300

    include all design patterns

---
+++ creational/factory-method/python/main.py
@@ -0,0 +1,15 @@
+from creator import *
+
+
+def client_code(creator: Creator) -> None:
+    print(f"Client: I'm not aware of the creator's class, but it still works.\n"
+          f"{creator.some_operation()}", end="")
+
+
+if __name__ == "__main__":
+    print("App: Launched with the ConcreteCreator1.")
+    client_code(ConcreteCreator1())
+    print("\n")
+
+    print("App: Launched with the ConcreteCreator2.")
+    client_code(ConcreteCreator2())
